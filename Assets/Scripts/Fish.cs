f8a41c0321c7ae5629ebd97c7fd1dc5899820e47
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-11-21 09:52:48 +0900

    Master

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [SerializeField] float jumpVelocity;
+    [SerializeField] float maxHeight;
+    [SerializeField] GameObject sprite;
+    Rigidbody2D rigid;
+    bool isDead;
+    public bool IsDead      //이렇게 일반 변수를 끼고 선언하면 내부에서만 변경 가능하고 외부에서는 읽기만 가능해진다.
+    {                       //즉, 내부에서는 isDead를 통해 IsDead에 쓰기가 가능하지만 외부에서는 IsDead를 읽기만 가능하다.
+        get
+        {
+            return isDead;
+        }
+    }
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeight)
+        {
+            if (!isDead && rigid.isKinematic == false)
+            {
+                Debug.Log("Jump!");
+                rigid.velocity = new Vector2(0.0f, jumpVelocity);
+            }
+        }
+
+        //물고기 회전
+        float angle;
+        if (isDead)
+        {
+            angle = 0f;
+        }
+        else
+        {
+            angle = Mathf.Atan2(rigid.velocity.y, 10) * Mathf.Rad2Deg;
+        }
+        sprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Camera.main.SendMessage("Shake");   //메인카메라는 Camera.main 찍어서 접근 가능하다.
+
+        isDead = true;
+    }
+
+    public void SetKinematic(bool value)
+    {
+        rigid.isKinematic = value;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    [SerializeField] Fish fish;
+    [SerializeField] GameObject pipes;
+    int score;
+
+    enum State
+    {
+        READY, PLAY, OVER
+    }
+    State state;
+
+    void Start()
+    {
+        pipes.SetActive(false);
+        state = State.READY;
+        fish.SetKinematic(true);
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.READY:
+                if (Input.GetButtonDown("Fire1")) GameStart();
+                break;
+            case State.PLAY:
+                if (fish.IsDead) GameOver();
+                break;
+            case State.OVER:
+                if (Input.GetButtonDown("Fire1")) SceneManager.LoadScene("Game");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void GameStart()
+    {
+        state = State.PLAY;
+
+        fish.SetKinematic(false);
+        pipes.SetActive(true);
+    }
+
+    void GameOver()
+    {
+        state = State.OVER;
+
+        ScrollObject[] scrollObjects = GameObject.FindObjectsOfType<ScrollObject>();
+
+        foreach (ScrollObject scrollObject in scrollObjects)
+        {
+            scrollObject.enabled = false;
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
